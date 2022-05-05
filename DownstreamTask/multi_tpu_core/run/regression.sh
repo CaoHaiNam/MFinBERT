07c3f47b664a2b42bf7511d60bda67d42922ca7d
namch <NamCH@cps.com.vn>
2022-05-05 10:22:07 +0700

    update sh file

--- DownstreamTask/multi_tpu_core/run/regression.sh
+++ DownstreamTask/multi_tpu_core/run/regression.sh
@@ -1,5 +1,5 @@
-# bert-base-multilingual-cased
 # bert-base-cased
+# bert-base-multilingual-cased
 accelerate launch DownstreamTask/multi_tpu_core/regression.py \
         --k_fold 10 \
         --model_name_or_path bert-base-multilingual-cased \
@@ -9,8 +9,10 @@ accelerate launch DownstreamTask/multi_tpu_core/regression.py \
         --max_seq_length 512 \
         --per_device_train_batch_size 8 \
         --per_device_eval_batch_size 16 \
+        --optim_strategy 1 \
+        --learning_rate 5e-5 \
         --lr_bert 5e-6 \
         --lr_fc 1e-3 \
-        --num_train_epochs 20 \
+        --num_train_epochs 10 \
         --freeze_layer_count 0 \
         --patience 3
